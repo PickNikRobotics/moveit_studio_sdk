2d63d2978832935e8e8862350ade1e6ce92d8ba8
Moth-Man <mothmanor@gmail.com>
2024-02-19 15:30:33 -0700

    Move shebang.. whoops.

--- moveit_studio_py/examples/start_blocking.py
+++ moveit_studio_py/examples/start_blocking.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Copyright 2023 Picknik Inc.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -25,8 +27,6 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-
-#!/usr/bin/env python3
 
 # Start an Objective and block until the Objective has completed execution.
 
